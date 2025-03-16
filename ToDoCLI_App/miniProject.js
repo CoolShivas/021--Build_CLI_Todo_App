8eb51f80e8532fe3761b84fff497666781b3e186
CoolShivas <shivas2710cool00@gmail.com>
2025-03-16 12:22:15 +0530

    Created a function named as showMenu and getting the Output on Terminal.

---
+++ ToDoCLI_App/miniProject.js
@@ -0,0 +1,18 @@
+
+const showMenu = () => {
+    console.log("1 : Add a Task");
+    console.log("2 : View Task");
+    console.log("3 : Exit");
+};
+showMenu();
+// // // // Here, we are getting the Output on Terminal as :-
+// // // // Open the Terminal and clear the stuff first.
+// // // // Check the list of folder in which you are working on. (i.e., by ls command).
+// // // // Then, enter into the folder named as ToDoCLI_App (i.e., by cd command).
+// // // // Check the list of folder named as ToDoCLI_App (i.e., by ls command).
+// // // // Finally, select the file named as miniProject.js to run or execute (i.e., node --watch miniProject.js)
+// // // // Therefore, we get the Output on Terminal as :-
+// // // // 1 : Add a Task
+// // // // 2 : View Task
+// // // // 3 : Exit
+// // // // Completed running 'miniProject.js'
